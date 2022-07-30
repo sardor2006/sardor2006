ab921801f03f27b28c9846dfa95380af41871ea0
sardor2006 <110012859+sardor2006@users.noreply.github.com>
2022-07-30 02:06:49 -0400

    Add files via upload

---
+++ Lesson/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int res = 1;
+        for (int a = 1; a <= 10; a++) {
+            res *= a;
+            System.out.println(res);
+
+        }
+    }
+}
